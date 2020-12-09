affd29b5f96fb5f1225d6b1896a496ab29e784eb
iKacperG <lumisworkshop@gmail.com>
2020-12-09 20:17:35 +0100

    add: context api provider, input change and submit fetch request logic, first name retrieved and showed, TODO: isolate to single responsibility

--- star-wars-browser/src/components/search_results_modal/SearchResultsModal.js
+++ star-wars-browser/src/components/search_results_modal/SearchResultsModal.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { PeopleDataContext } from '../../providers/peopleData/PeopleDataContext';
+
 
 const SearchResultsModal = () => {
+
+    const {peopleData,setPeopleData} = useContext(PeopleDataContext)
+
     return (
         <div>
-            
+            <div className='test'>{peopleData[0]?.name}</div>    
         </div>
     )
 }
