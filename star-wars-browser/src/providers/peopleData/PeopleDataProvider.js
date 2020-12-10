0d8b3c3329aa4fb2bd6bed4d17d10c8ca422a6b6
iKacperG <lumisworkshop@gmail.com>
2020-12-10 23:04:14 +0100

    add: rough fetching logic sketch for people and planets, TODO: cleanup

--- star-wars-browser/src/providers/peopleData/PeopleDataProvider.js
+++ star-wars-browser/src/providers/peopleData/PeopleDataProvider.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {PeopleDataContext} from "./PeopleDataContext";
 const PeopleDataProvider = ({children}) => {
 
-    const [peopleData,setPeopleData] = useState([])
+    const [peopleData,setPeopleData] = useState([[]])
 
     return (
         <PeopleDataContext.Provider value={{peopleData,setPeopleData}}>
