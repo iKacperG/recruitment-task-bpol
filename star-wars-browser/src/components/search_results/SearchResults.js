8b2a3b0e3d58235cf5c9933931e352e4e5bdcccb
iKacperG <lumisworkshop@gmail.com>
2020-12-13 13:50:39 +0100

    fix: d-none added to labels

--- star-wars-browser/src/components/search_results/SearchResults.js
+++ star-wars-browser/src/components/search_results/SearchResults.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, {useState, useEffect, useContext } from 'react';
 
 import { PeopleDataContext } from '../../providers/peopleData/PeopleDataContext';
 import { CurrentPageContext } from '../../providers/currentPage/CurrentPageContext';
@@ -11,12 +11,18 @@ const SearchResults = () => {
     const { currentPage, setCurrentPage } = useContext(CurrentPageContext);
     const { peopleData,setPeopleData } = useContext(PeopleDataContext);
 
+    const [ labelClassName, setLabelClassName ] = useState('search-results-label__list d-none');
+
     const pagination = paginate(currentPage, setCurrentPage, peopleData);
       
+    useEffect(() => {
+        if(peopleData.length > 1) setLabelClassName('search-results-label__list'); 
+    }, [peopleData])
+
   return (
   
         <>
-            <ul className='search-results-label__list'>
+            <ul className={labelClassName}>
                 <li className='search-results-label__element'>Name</li>
                 <li className='search-results-label__element'>Homeland</li>
                 <li className='search-results-label__element'>Population</li>
